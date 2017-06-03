df52a6bbe449e6c0f142bed125b6e259bb546622
Kelvin Medina <kelvinm2423@gmail.com>
2017-06-03 11:33:58 -0400

    New DB

--- db/schema.sql
+++ db/schema.sql
@@ -1,38 +1,36 @@
-CREATE DATABASE puppylove;
+ CREATE DATABASE puppylove;
 
-USE puppylove;
+ USE puppylove;
 
-CREATE TABLE dogs(
-  item_id INT AUTO_INCREMENT NOT NULL,
-  breed VARCHAR(45) NOT NULL,
-  age DECIMAL(20) NOT NULL,
-  sex VARCHAR(45) NOT NULL,
-  primary key(item_id)
-);
+ CREATE TABLE puppy(
+   item_id INT AUTO_INCREMENT NOT NULL,
+   breed VARCHAR(45) NOT NULL,
+   age DECIMAL(20) NOT NULL,
+   gender VARCHAR(45) NOT NULL,
+   image varchar(500) NOT NULL,
+   ownerFirstName varchar(100) NOT NULL,
+   ownerLastName varchar(100) NOT NULL,
+   ownerEmail varchar(200) NOT NULL,
+   ownerAddress varchar(200) NOT NULL,
+   ownerCity varchar(100) NOT NULL,
+   ownerState varchar(50) NOT NULL,
+   ownerZipcode varchar(5) NOT NULL,
+   primary key(item_id)
+  );
 
-select * from dogs;
+select * from puppy;
 
-INSERT INTO dogs (breed, age, sex)
-VALUES ("Pitbull", 2, "male"),
-  ("Shih-Tzu", 6, "female"),
-  ("Golden Retriever", 8, "male"),
-  ("German Shepard", 10, "female"),
-  ("English Bulldog", 5, "male"),
-  ("Yorkie", 7, "female");
+INSERT INTO puppy (breed, age, gender, image, ownerFirstName, ownerLastName, ownerEmail, ownerAddress, ownerCity, ownerState, ownerZipcode)
+  VALUES ("Pitbull", 2, "male", "http://bit.ly/2sB7uw6", "Bob", "Smith", "bob@gmail.com", "1234 SW 34th St.", "New York", "NY" , 10012),
+    ("Shih-Tzu", 6, "female", "http://bit.ly/2sB7FaK", "Jessica", "Garcia", "jessica@yahoo.com", "45 NW 4th Blvd.", "Chicago", "IL" , 60018),
+    ("Golden Retriever", 8, "male", "http://bit.ly/2s4us2a", "Carlos", "Robinson", "cr17@gmail.com", "678 W 24th Ave.", "Phoenix", "AZ" , 85001),
+    ("German Shepard", 10, "female", "http://bit.ly/2rQiLfy", "Abby", "Rauss", "abbyrauss@yahoo.com", "900 S 54th St.", "Miami", "FL" , 33018),
+    ("English Bulldog", 5, "male", "http://bit.ly/2s4LXPX", "Sam", "Cheeks", "sammyc@aol.com", "1000 NE 96th Ct.", "Los Angeles", "CA" , 90007),
+    ("Yorkie", 7, "female", "http://bit.ly/2g8Xa8Y", "Jack", "Redshield", "jredshield@gmail.com", "4321 SW 110th Blvd.", "Sacramento", "CA" , 94203); 
 
-CREATE TABLE humans(
-  item_id INT AUTO_INCREMENT NOT NULL,
-  human_name VARCHAR(45) NOT NULL,
-  zipcode DECIMAL(5) NOT NULL,
-);
 
-select * from humans;
+  
+  
+  
 
-INSERT INTO humans (human_name, zipcode)
-VALUES ("John", 33071),
-  ("Bob", 33065),
-  ("Sarah", 33018),
-  ("Linda", 33132),
-  ("Adam", 33004),
-  ("Josh", 33009);
 
